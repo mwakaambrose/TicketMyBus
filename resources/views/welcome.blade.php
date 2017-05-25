4f5cc1cd93c5786e9d0d6f02a8f5887fd726dd51
Mwaka Ambrose <ambrosemwaka@gmail.com>
2017-05-25 19:15:15 +0300

    adds some styling

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,21 +3,19 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-10 col-md-offset-1">
-        	<div class="row">
-        		<div class="col-md-8">
-        			<img src="img/homelandbus.JPG" alt="bus" class="img img-responsive img-rounded">
-        		</div>
-        		<div class="col-md-4">
-        			<h3>Welcome to TicketMyBus</h3>
-		            <div class="heading-to-margin">
-		                <h5>
-		                	Homeland Holdings Online bus ticketing app.
-		                	This is an admin web appliction for approving and canceling travels.
-		                </h5>
-		            </div>
-        		</div>
-        	</div>
+        <div class="col-md-12 top-margin">
+    		<div class="col-md-8">
+    			<img src="img/homelandbus.JPG" alt="bus" class="img img-custom img-responsive img-rounded">
+    		</div>
+    		<div class="col-md-4">
+    			<h3 class="top-margin-h">Welcome to TicketMyBus</h3>
+	            <div>
+	                <h5>
+	                	Homeland Holdings Online bus ticketing app.
+	                	This is an admin web appliction for approving and canceling travels.
+	                </h5>
+	            </div>
+    		</div>
         </div>
     </div>
 </div>
