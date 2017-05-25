3cc18ea80f08713e590b29f2eee61ddbbfac03b6
Mwaka Ambrose <ambrosemwaka@gmail.com>
2017-05-25 19:19:55 +0300

    adds some styling and footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -75,6 +75,10 @@
     </nav>
 
     @yield('content')
+    <hr>    
+    <<footer>
+        <center>&copy 2017 Gulu University Alumni Students</center>
+    </footer>
 
     <!-- JavaScripts -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.3/jquery.min.js" integrity="sha384-I6F5OKECLVtK/BL+8iSLDEHowSAfUo76ZL9+kGAgTRdiByINKJaqTPH/QVNS1VDb" crossorigin="anonymous"></script>
