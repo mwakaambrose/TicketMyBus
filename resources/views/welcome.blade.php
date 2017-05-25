0712d87cd758006fc762ab14eb050fdcb0e6d1c7
Mwaka Ambrose <ambrosemwaka@gmail.com>
2017-05-25 18:58:44 +0300

    adds images

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,11 +4,18 @@
 <div class="container">
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
-            <h3>Welcome to TicketMyBus</h3>
-
-            <div class="heading-to-margin">
-                Your Application's Landing Page.
-            </div>
+        	<div class="row">
+        		<div class="col-md-8"></div>
+        		<div class="col-md-4">
+        			<h3>Welcome to TicketMyBus</h3>
+		            <div class="heading-to-margin">
+		                <h5>
+		                	Homeland Holdings Online bus ticketing app.
+		                	This is an admin web appliction for approving and canceling travels.
+		                </h5>
+		            </div>
+        		</div>
+        	</div>
         </div>
     </div>
 </div>
